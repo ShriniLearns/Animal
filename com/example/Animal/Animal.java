d06fa36c52e1d58ed96b6034f634f7a55eb6039f
Shrinidhi G <shrinidhikaranth95@gmail.com>
2023-06-27 15:42:14 +0530

    Animal properties is commited

---
+++ com/example/Animal/Animal.java
@@ -0,0 +1,23 @@
+package com.example.Animal;
+
+public class Animal {
+    String color;
+    int age;
+    int weight;
+    void info(String a, int b, int c)
+    {
+        color = a;
+        age = b;
+        weight = c;
+    }
+    void display()
+    {
+        System.out.println(color+" "+age+" "+weight);
+    }
+    public static void main(String[] args)
+    {
+        Animal guzo = new Animal();
+        guzo.info("white", 6, 35);
+        guzo.display();
+    }
+}
