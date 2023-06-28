a2017d556ce68fa451a7fe7ebf8c9c794d1476c6
shrinidhi-demo <136307370+shrinidhi-demo@users.noreply.github.com>
2023-06-28 15:19:03 +0530

    Update upload_artifact.sh

--- .github/workflows/upload_artifact.sh
+++ .github/workflows/upload_artifact.sh
@@ -16,4 +16,4 @@ curl -X POST \
   -H "Authorization: token $access_token" \
   -H "Content-Type: application/octet-stream" \
   --data-binary "@$file_path" \
-  "https://uploads.github.com/repos/$repository_owner/$repository_name/contents/$file_path"
+  "https://api.github.com/$repository_owner/$repository_name/repos/contents/$file_path"
