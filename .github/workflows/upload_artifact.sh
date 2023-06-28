7114911c18f0796b8912b3344da0e75ec131e24d
shrinidhi-demo <136307370+shrinidhi-demo@users.noreply.github.com>
2023-06-28 14:45:00 +0530

    Create upload_artifact.sh

---
+++ .github/workflows/upload_artifact.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# File path and name
+file_path="/Users/shrinidhigcw/IdeaProjects/Animal/pom.xml"
+#file_name="file.txt"
+
+# GitHub repository information
+repository_owner="shrinidhi-demo"
+repository_name="Animal"
+
+# Personal access token (PAT) with repository scope
+access_token="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"
+
+# Upload file to GitHub repository using curl
+curl -X POST \
+  -H "Authorization: token $access_token" \
+  -H "Content-Type: application/octet-stream" \
+  --data-binary "@$file_path" \
+  "https://uploads.github.com/repos/$repository_owner/$repository_name/contents/$file_path"
