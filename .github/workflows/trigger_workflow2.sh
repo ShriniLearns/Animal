6a605da5855cfd8bb7cd8e822ddb86dfc8c84c0c
shrinidhi-demo <shrinidhikaranth95@gmail.com>
2023-07-05 12:43:17 +0530

    Error set to fail the job.

--- .github/workflows/trigger_workflow2.sh
+++ .github/workflows/trigger_workflow2.sh
@@ -4,7 +4,7 @@ set -e
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer 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" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/shrinidhi-demo/Animal/actions/workflows/workflow2.yml/dispatches \
   -d '{"ref":"main"}'
