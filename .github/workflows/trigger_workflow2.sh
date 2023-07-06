24f36ef7794094e0aa3e3d7451996ee0d6797565
shrinidhi-demo <shrinidhikaranth95@gmail.com>
2023-07-06 15:21:03 +0530

    reuqired changes have been made

--- .github/workflows/trigger_workflow2.sh
+++ .github/workflows/trigger_workflow2.sh
@@ -15,7 +15,7 @@
   curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${{ secrets.ADMIN_TOKEN }}" \
+  -H "Authorization: Bearer $ADMIN_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  "https://api.github.com/repos/${{ github.repository }}/actions/workflows/workflow2.yml/dispatches" \
+  "https://api.github.com/repos/shrinidhi-demo/Animal/actions/workflows/workflow2.yml/dispatches" \
   -d '{"ref":"main"}'
