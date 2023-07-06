576fdb787e7396dc552870771eaa59acc3332b07
shrinidhi-demo <shrinidhikaranth95@gmail.com>
2023-07-06 14:33:48 +0530

    reuqired changes have been made

--- .github/workflows/trigger_workflow2.sh
+++ .github/workflows/trigger_workflow2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-set -e
+
 # Trigger the child workflow using the GitHub API
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer 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" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/shrinidhi-demo/Animal/actions/workflows/workflow2.yml/dispatches \
   -d '{"ref":"main"}'
+
