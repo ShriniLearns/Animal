d1cd954188b68d9bdf67a4b3929f8b982abc6007
shrinidhi-demo <shrinidhikaranth95@gmail.com>
2023-07-06 15:16:28 +0530

    reuqired changes have been made

--- .github/workflows/trigger_workflow2.sh
+++ .github/workflows/trigger_workflow2.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# Retrieve the value of the environment variable 'Demo1'
-GITHUB_TOKEN="$Demo1"
+# Assign the value of the environment variable 'Demo1' to 'GITHUB_TOKEN'
+#GITHUB_TOKEN="$Demo1"
+#
+## Trigger the child workflow using the GitHub API
+#curl -L \
+#  -X POST \
+#  -H "Accept: application/vnd.github+json" \
+#  -H "Authorization: Bearer $GITHUB_TOKEN" \
+#  -H "X-GitHub-Api-Version: 2022-11-28" \
+#  https://api.github.com/repos/shrinidhi-demo/Animal/actions/workflows/workflow2.yml/dispatches \
+#  -d '{"ref":"main"}'
 
-# Trigger the child workflow using the GitHub API
-curl -L \
+  curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "Authorization: Bearer ${{ secrets.ADMIN_TOKEN }}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/shrinidhi-demo/Animal/actions/workflows/workflow2.yml/dispatches \
+  "https://api.github.com/repos/${{ github.repository }}/actions/workflows/workflow2.yml/dispatches" \
   -d '{"ref":"main"}'
